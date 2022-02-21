e7b12a281723279362699cc182d6c3211e734f59
alisam1 <alisa-m2@yandex.ru>
2022-02-21 15:50:20 +0300

    Module-6/Task-1-2-3

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,28 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.createTable(rows);
   }
+  createTable(rows) {
+    const table = document.createElement('table');
+    this.tbody = table.createTBody('tbody');
+    for (const rowTable of this.rows) {
+      this.tbody.insertAdjacentHTML('beforeEnd', `<tr>
+				<td>${rowTable.name}</td>
+				<td>${rowTable.age}</td>
+        <td>${rowTable.salary}</td>
+        <td>${rowTable.city}</td>
+        <td><button>X</button></td>
+			</tr>`);
+    }
+
+    let button = this.tbody.querySelectorAll('button');
+    button.forEach(item => {
+      item.addEventListener('click', (event) =>
+        event.target.closest("tr").remove());
+    });
+
+    return table;
+  }
+
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,58 @@
 export default class ProductCard {
   constructor(product) {
+    this.data = product;
+    this.elem = this.createCard(product);
+  }
+
+  createCard(product) {
+    const divContainer = document.createElement('div');
+    divContainer.classList.add('container_half');
+
+    const divCard = document.createElement('div');
+    divCard.classList.add('card');
+
+    const divCardTop = document.createElement('div');
+    divCardTop.classList.add('card__top');
+
+    const divCardPrice = document.createElement('span');
+    divCardPrice.classList.add('card__price');
+    divCardPrice.innerHTML = '€' + this.data.price.toFixed(2);
+
+    const divCardImg = document.createElement('img');
+    divCardImg.classList.add('card__image');
+    divCardImg.src = '../../assets/images/products/' + this.data.image;
+
+    const divCardBody = document.createElement('div');
+    divCardBody.classList.add('card__body');
+
+    const divCardTitle = document.createElement('div');
+    divCardTitle.classList.add('card__title');
+    divCardTitle.innerHTML = this.data.name;
+
+    const divCardButton = document.createElement('button');
+    divCardButton.classList.add('card__button');
+    const divCardButtonImg = document.createElement('img');
+    divCardButtonImg.src = '../../assets/images/icons/plus-icon.svg';
+    divCardButton.append(divCardButtonImg);
+
+    divCardButton.addEventListener('click', () => {
+      console.log(this.data.id);
+
+      const addProduct = new CustomEvent("product-add", {
+        detail: this.data.id,
+        bubbles: true
+      });
+
+      divCardButton.dispatchEvent(addProduct);
+    });
+
+    divContainer.append(divCard);
+    divCard.append(divCardTop);
+    divCardTop.append(divCardPrice);
+    divCardTop.append(divCardImg);
+    divCard.append(divCardBody);
+    divCardBody.append(divCardTitle);
+    divCardBody.append(divCardButton);
+    return divCard;
   }
 }
